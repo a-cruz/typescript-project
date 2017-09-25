7eb35adc1e191a7da164c7d816850852c260f272
a-cruz <geline.cruz1598@gmail.com>
2017-09-25 13:46:24 +0800

    first commit

---
+++ greeter.ts
@@ -0,0 +1,6 @@
+function greeter(person)
+	{
+		return "Hello, " + person;
+	}
+var user="James Esquivel";
+document.body.innerHTML = greeter(user);
---
+++ tip.js
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>TypeScript</title>
+		<script type="text/javascript" src="greetme.js"></script>
+		<script type="text/javascript" src="factorial.js"></script>
+	</head>
+	
+	<body>
+		<h1 style="text-align:center">TypeScript Sample Codes</h1>
+		<div style = "width:20%; text-align:center; float:left clear:both">
+		<button onclick="greetme()">Display Greeter</button><br/>
+		<button onclick="fact()">Display Factorial</button><br/>
+		
+		<fieldset>
+			<legend>Output Display</legend>
+			<p id="f"></p>
+		</fieldset>
+		</div>
+		
+		<footer>
+		<div>
+			Codes By: CIL <br/>
+			Date Submitted:
+			<script language="javascript">
+			  var today = new Date();
+			</script>
+		</div>
+		</footer>
+	</body>
+</html>
